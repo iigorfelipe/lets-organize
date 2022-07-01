57494426af84f9924193b8d83fe5094f6a282318
Igor <iigorfelipe@gmail.com>
2022-07-01 16:12:46 -0300

    test: testa se os valores do select estão na tela

--- app/frontend/src/tests/App.test.js
+++ app/frontend/src/tests/App.test.js
@@ -1,9 +1,17 @@
-// import { render, screen } from '@testing-library/react'
-// import React from 'react'
-// import App from '../App'
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import App from '../App'
 
-// test('renders learn react link', () => {
-//   render(<App />)
-//   const linkElement = screen.getByText(/hello world/i)
-//   expect(linkElement).toBeInTheDocument()
-// })
+describe('Visualização da aplicação', () => {
+  render(<App />)
+
+  test('É esperado que os valores do select estão corretos', () => {
+    const stts = screen.getByText(/status/i)
+    const abcd = screen.getByText(/alfabética/i)
+    const data = screen.getByText(/data de criação/i)
+
+    expect(stts).toBeInTheDocument()
+    expect(abcd).toBeInTheDocument()
+    expect(data).toBeInTheDocument()
+  })
+})
