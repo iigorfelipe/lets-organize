581dd04331fb9ed4eda3bad26451dc10cc4441c6
Igor <iigorfelipe@gmail.com>
2022-07-01 22:15:48 -0300

    refactor: agora será um describe para cada componente

--- app/frontend/src/tests/App.test.js
+++ app/frontend/src/tests/App.test.js
@@ -3,64 +3,62 @@ import userEvent from '@testing-library/user-event'
 import React from 'react'
 import App from '../App'
 
-describe('ButtonsUp Component', () => {
-  describe('É esperado que:', () => {
-    beforeEach(() => render(<App />))
+beforeEach(() => render(<App />))
 
-    test('o input de adicionar uma tarefa seja renderizado', () => {
-      const stts = screen.getByPlaceholderText(/digite uma tarefa/i)
+describe('É esperado em ButtonsUp que:', () => {
+  test('o input de adicionar uma tarefa seja renderizado', () => {
+    const stts = screen.getByPlaceholderText(/digite uma tarefa/i)
 
-      expect(stts).toBeInTheDocument()
-    })
+    expect(stts).toBeInTheDocument()
+  })
 
-    test('o botão de adicionar uma tarefa seja renderizado', () => {
-      const btnAdd = screen.getByTestId('input-add')
+  test('o botão de adicionar uma tarefa seja renderizado', () => {
+    const btnAdd = screen.getByTestId('input-add')
 
-      expect(btnAdd).toBeInTheDocument()
-    })
+    expect(btnAdd).toBeInTheDocument()
+  })
 
-    test('os valores do select foram renderizados', () => {
-      const stts = screen.getByText(/status/i)
-      const abcd = screen.getByText(/alfabética/i)
-      const data = screen.getByText(/data de criação/i)
+  test('os valores do select foram renderizados', () => {
+    const stts = screen.getByText(/status/i)
+    const abcd = screen.getByText(/alfabética/i)
+    const data = screen.getByText(/data de criação/i)
 
-      expect(stts).toBeInTheDocument()
-      expect(abcd).toBeInTheDocument()
-      expect(data).toBeInTheDocument()
-    })
+    expect(stts).toBeInTheDocument()
+    expect(abcd).toBeInTheDocument()
+    expect(data).toBeInTheDocument()
+  })
 
-    test('não tenha checkbox renderizado', () => {
-      const checkbox = screen.queryByRole('checkbox')
+  test('não tenha checkbox renderizado', () => {
+    const checkbox = screen.queryByRole('checkbox')
 
-      expect(checkbox).not.toBeInTheDocument()
-    })
+    expect(checkbox).not.toBeInTheDocument()
+  })
 
-    test('o botão de adicionar uma tarefa esteja desativado', () => {
-      const btnAdd = screen.getByTestId('btn-add')
+  test('o botão de adicionar uma tarefa esteja desabilitado', () => {
+    const btnAdd = screen.getByTestId('btn-add')
 
-      expect(btnAdd).toBeDisabled()
-    })
+    expect(btnAdd).toBeDisabled()
+  })
 
-    test('o botão de adicionar uma tarefa é habilitado caso o input tenha um valor', () => {
-      const inputAdd = screen.getByTestId('input-add')
-      const btnAdd = screen.getByTestId('btn-add')
+  test('o botão de adicionar uma tarefa é habilitado caso o input tenha um valor', () => {
+    const inputAdd = screen.getByTestId('input-add')
+    const btnAdd = screen.getByTestId('btn-add')
 
-      userEvent.type(inputAdd, 'one piece')
+    userEvent.type(inputAdd, 'one piece')
 
-      expect(btnAdd).not.toBeDisabled()
-    })
+    expect(btnAdd).not.toBeDisabled()
+  })
 
-    test('seja possível adicionar uma tarefa', () => {
-      const inputAdd = screen.getByTestId('input-add')
-      const btnAdd = screen.getByTestId('btn-add')
+  test('seja possível adicionar uma tarefa', () => {
+    const inputAdd = screen.getByTestId('input-add')
+    const btnAdd = screen.getByTestId('btn-add')
 
-      userEvent.type(inputAdd, 'one piece')
-      userEvent.click(btnAdd)
-      userEvent.type(inputAdd, 'fçlakhfla')
-      userEvent.click(btnAdd)
+    userEvent.type(inputAdd, 'one piece')
+    userEvent.click(btnAdd)
+    userEvent.type(inputAdd, 'fçlakhfla')
+    userEvent.click(btnAdd)
 
-      expect(screen.getByText('one piece')).toBeInTheDocument()
-      expect(screen.getByText('fçlakhfla')).toBeInTheDocument()
-    })
+    expect(screen.getByText('one piece')).toBeInTheDocument()
+    expect(screen.getByText('fçlakhfla')).toBeInTheDocument()
   })
 })
