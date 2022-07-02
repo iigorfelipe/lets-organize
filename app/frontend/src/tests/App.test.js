c36dc54d0f7aa2b2144eac1f4890efe9481cdf35
Igor <iigorfelipe@gmail.com>
2022-07-01 21:13:55 -0300

    refactor: altera a ordem de exibição dos testes e adiciona mais um describe

--- app/frontend/src/tests/App.test.js
+++ app/frontend/src/tests/App.test.js
@@ -3,47 +3,49 @@ import userEvent from '@testing-library/user-event'
 import React from 'react'
 import App from '../App'
 
-describe('ButtonsUp Component. É esperado que:', () => {
-  beforeEach(() => render(<App />))
+describe('ButtonsUp Component', () => {
+  describe('É esperado que:', () => {
+    beforeEach(() => render(<App />))
 
-  test('os valores do select estão corretos', () => {
-    const stts = screen.getByText(/status/i)
-    const abcd = screen.getByText(/alfabética/i)
-    const data = screen.getByText(/data de criação/i)
+    test('o input de adicionar uma tarefa seja renderizado', () => {
+      const stts = screen.getByPlaceholderText(/digite uma tarefa/i)
 
-    expect(stts).toBeInTheDocument()
-    expect(abcd).toBeInTheDocument()
-    expect(data).toBeInTheDocument()
-  })
+      expect(stts).toBeInTheDocument()
+    })
 
-  test('o input de adicionar uma tarefa seja renderizado', () => {
-    const stts = screen.getByPlaceholderText(/digite uma tarefa/i)
+    test('o botão de adicionar uma tarefa seja renderizado', () => {
+      const btnAdd = screen.getByTestId('input-add')
 
-    expect(stts).toBeInTheDocument()
-  })
+      expect(btnAdd).toBeInTheDocument()
+    })
 
-  test('o botão de adicionar uma tarefa seja renderizado', () => {
-    const btnAdd = screen.getByTestId('input-add')
+    test('os valores do select foram renderizados', () => {
+      const stts = screen.getByText(/status/i)
+      const abcd = screen.getByText(/alfabética/i)
+      const data = screen.getByText(/data de criação/i)
 
-    expect(btnAdd).toBeInTheDocument()
-  })
+      expect(stts).toBeInTheDocument()
+      expect(abcd).toBeInTheDocument()
+      expect(data).toBeInTheDocument()
+    })
 
-  test('não tenha checkbox renderizado', () => {
-    const checkbox = screen.queryByRole('checkbox')
+    test('não tenha checkbox renderizado', () => {
+      const checkbox = screen.queryByRole('checkbox')
 
-    expect(checkbox).not.toBeInTheDocument()
-  })
+      expect(checkbox).not.toBeInTheDocument()
+    })
 
-  test('seja possível adicionar uma tarefa', () => {
-    const inputAdd = screen.getByTestId('input-add')
-    const btnAdd = screen.getByTestId('btn-add')
+    test('seja possível adicionar uma tarefa', () => {
+      const inputAdd = screen.getByTestId('input-add')
+      const btnAdd = screen.getByTestId('btn-add')
 
-    userEvent.type(inputAdd, 'one piece')
-    userEvent.click(btnAdd)
-    userEvent.type(inputAdd, 'fçlakhfla')
-    userEvent.click(btnAdd)
+      userEvent.type(inputAdd, 'one piece')
+      userEvent.click(btnAdd)
+      userEvent.type(inputAdd, 'fçlakhfla')
+      userEvent.click(btnAdd)
 
-    expect(screen.getByText('one piece')).toBeInTheDocument()
-    expect(screen.getByText('fçlakhfla')).toBeInTheDocument()
+      expect(screen.getByText('one piece')).toBeInTheDocument()
+      expect(screen.getByText('fçlakhfla')).toBeInTheDocument()
+    })
   })
 })
